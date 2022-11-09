dfd8a7244680924dc118d55a44c5ece889fe6ad2
kkimDev-01 <kkheentech@gmail.com>
2022-11-10 01:18:12 +0900

    auto-push

---
+++ src/com/baekjoon/prob15649.java
@@ -0,0 +1,4 @@
+package com.baekjoon;
+
+public class prob15649 {
+}
---
+++ src/com/baekjoon/prob1676.java
@@ -0,0 +1,26 @@
+package com.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class prob1676 {
+}
+
+class prob1676_1{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        int count = 0;
+
+        while (num >= 5) {
+            count += num / 5;
+            num /= 5;
+        }
+
+        System.out.println(count);
+
+    }
+}
