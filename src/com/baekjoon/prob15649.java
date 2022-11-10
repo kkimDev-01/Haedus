acf391542ca907ab71dbe9ce489089ad4e3f73fa
kkimDev-01 <kkheentech@gmail.com>
2022-11-11 02:45:37 +0900

    auto-push

--- src/com/baekjoon/prob15649.java
+++ src/com/baekjoon/prob15649.java
@@ -1,4 +1,110 @@
 package com.baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+문제 : https://www.acmicpc.net/problem/15649
+해설 : https://st-lab.tistory.com/114
+정리내용(블로그)
+    -  :
+    -  :
+    -  :
+*/
 public class prob15649 {
+}
+
+//디버깅으로 따라가면서 이해한 수준...
+//BufferedReader + StringBuilder
+class prob15649_1 {
+
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visit = new boolean[N];
+        dfs(N, M, 0);
+        System.out.println(sb);
+
+    }
+
+    public static void dfs(int N, int M, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(N, M, depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+
+
+}
+
+//[BufferedReader + StringBuilder + N과 M 전역변수화]
+class prob15649_2 {
+
+    public static int N;	// 정적변수로 변경
+    public static int M;	// 정적변수로 변경
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 정적변수 N과 M을 초기화해준다.
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visit = new boolean[N];
+
+        // 정적변수를 쓰면 되기 때문에 굳이 N과 M을 넘겨줄 필요 없다.
+        dfs(0);
+        System.out.println(sb);
+
+    }
+
+    public static void dfs(int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
 }
