b4bc79a3388038b96295da9caea983d0c71b371a
kkimDev-01 <kkheentech@gmail.com>
2022-11-22 01:21:58 +0900

    auto-push

---
+++ src/com/leetcode/twoSum.java
@@ -0,0 +1,58 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+public class twoSum {
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        System.out.println(twoSumSol(nums, target));
+
+
+    }
+
+
+
+    public static int[] twoSumSol(int[] nums, int target) {
+
+        int[] numsClone = nums.clone();
+
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = nums.length-1;
+
+        int idx1 = 0;
+        int idx2 = 0;
+
+        while (left < right) {
+            if (nums[left] + nums[right] == target) {
+                for (int i = 0; i < numsClone.length; i++) {
+                    if (numsClone[i] == nums[left]) {
+                        idx1 = i;
+                    }
+                }
+                for (int i = numsClone.length -1; i >= 0; i--) {
+                    if (numsClone[i] == nums[right]) {
+                        idx2 = i;
+                    }
+                }
+                break;
+            }
+            else if (nums[left] + nums[right] > target) {
+                right--;
+            }
+            else {
+                left++;
+            }
+        }
+
+        int[] res = new int[2];
+        res[0] = idx1;
+        res[1] = idx2;
+
+        return res;
+    }
+}
