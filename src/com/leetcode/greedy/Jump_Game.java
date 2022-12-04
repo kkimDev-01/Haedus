fd0c0a6d09abbdf9b3523ef4ef55944bb45a0301
kkimDev-01 <kkheentech@gmail.com>
2022-12-04 17:36:35 +0900

    auto-push

---
+++ src/com/leetcode/greedy/Jump_Game.java
@@ -0,0 +1,21 @@
+package com.leetcode.greedy;
+
+public class Jump_Game {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(canJump(nums));
+    }
+
+    public static boolean canJump(int[] nums) {
+        int reach = 0;
+        for(int i=0;i<=reach;i++){
+            reach= Math.max(reach , i+nums[i]);
+            if(reach>=nums.length-1){
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
