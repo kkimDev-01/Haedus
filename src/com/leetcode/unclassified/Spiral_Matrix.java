d842053b90473ff039f6afd86f1ee6c75459ec40
kkimDev-01 <kkheentech@gmail.com>
2022-12-04 16:50:49 +0900

    auto-push

--- src/com/leetcode/unclassified/Spiral_Matrix.java
+++ src/com/leetcode/unclassified/Spiral_Matrix.java
@@ -12,113 +12,103 @@ public class Spiral_Matrix {
 
     public static void main(String[] args) {
 
-//        matrix = [[1,2,3],[4,5,6],[7,8,9]]
-
-
+// matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// [1,2,3,6,9,8,7,4,5]
 
         //piralOrder(matrix);
 
     }
 
 
-    public static boolean end;
+    public static enum Direction {
+        RIGHT,
+        LEFT,
+        UP,
+        DOWN
+    }
+
     public static int X;
     public static int Y;
-    public static HashMap<String, Boolean> map;
-    public static List<Integer> list;
 
-    public static String heading;
+    public static HashMap<String, Boolean> map;
+
+    public static boolean end;
+
+    public static int[][] mat;
+
+    public static Direction heading;
 
     public static List<Integer> spiralOrder(int[][] matrix) {
-        end = false;
         X = 0;
         Y = 0;
         map = new HashMap<>();
-        list = new ArrayList<>();
-        heading = "r"; //right
+        end = false;
+        mat = matrix;
+        heading = Direction.RIGHT;
 
+        List<Integer> list = new ArrayList<>();
 
-        list.add(matrix[X][Y]);
+        map.put(X + "-" + Y, true);
+        list.add(mat[0][0]);
 
-        while(!end) {
-            proceed(matrix);
+        while(!(map.size() == matrix.length * matrix[0].length)) {
+            boolean headSucc = false;
+            boolean rightSucc = false;
+            headSucc = head();
+            if (headSucc){
+                map.put(X + "-" + Y, true);
+                list.add(mat[Y][X]);
+            }else {
+                right();
+            }
+
         }
 
         return list;
+    }
+
+    private static void right() {
+        if (heading == Direction.RIGHT){
+            heading = Direction.DOWN;
+        } else if (heading == Direction.LEFT) {
+            heading = Direction.UP;
+
+        } else if (heading == Direction.UP) {
+            heading = Direction.RIGHT;
+
+        } else if (heading == Direction.DOWN) {
+            heading = Direction.LEFT;
+        }
+    }
+
+
+    private static boolean head() {
+        boolean flag = false;
+
+        if (heading == Direction.RIGHT){
+            if (X+1 < mat[0].length && !map.containsKey((X+1)+"-"+Y)){
+                X++;
+                return true;
+            }
+        } else if (heading == Direction.LEFT && !map.containsKey((X-1)+"-"+Y)) {
+            if (X-1 >= 0){
+                X--;
+                return true;
+            }
+        } else if (heading == Direction.UP && !map.containsKey((X)+"-"+(Y-1))) {
+            if (Y-1 >= 0){
+                Y--;
+                return true;
+            }
+        } else if (heading == Direction.DOWN && !map.containsKey((X)+"-"+(Y+1))) {
+            if (Y+1 < mat.length){
+                Y++;
+                return true;
+            }
+        }
+        return false;
 
     }
 
-    private static void proceed(int[][] matrix) {
-
-
-
-
-        boolean firstTry = tryHeading(heading);
-
-        if (!firstTry) {
-            tryGeneral(matrix);
-        }
-
-
-        if (!end) {
-            list.add(matrix[Y][X]);
-        }
-
-
-    }
-
-    private static boolean tryHeading(String heading) {
-
-        switch(heading) {
-            case "L":
-                break;
-            case "U":
-
-
-                break;
-            case "D":
-
-
-                break;
-            default :
-
-                break;
-        }
-
-        return true;
-    }
-
-    private static void tryGeneral(int[][] matrix) {
-
-        int rightX = X+1;
-        int leftX = X-1;
-        int upY = Y-1;
-        int downY = Y+1;
-
-        //try right
-        if (rightX < matrix[0].length && !map.containsKey(rightX + "-" + Y)) {
-            map.put(rightX + "-" + Y, true);
-            X = rightX;
-        }
-        //try down
-        else if (downY < matrix.length && !map.containsKey(X+"-"+downY)){
-            map.put(X+"-"+downY, true);
-            Y = downY;
-        }
-        //try left
-        else if (leftX >= 0 && !map.containsKey(leftX+"-"+Y)){
-            map.put(leftX+"-"+Y, true);
-            X = leftX;
-        }
-        //try up
-        else if (upY >=0 && !map.containsKey(X+"-"+upY)){
-            map.put(X+"-"+upY, true);
-            Y = upY;
-        }
-        else {
-            end = true;
-        }
-
-    }
 
 }
